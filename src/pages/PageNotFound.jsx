68c70c0022b69446c5eeebf1dcb85a74678fa4f8
sambit1708 <sambitkhandai6@gmail.com>
2023-07-11 11:54:58 +0530

    pageNotFound ImgLink

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PageNotFound = () => {
   return (
     <div>
-        <img style={{ height:'661px', width:'1365px' }} src='"https://drive.google.com/uc?export=view&id=12S0JQ-ri_NoDbrDuKntzabEUXHFnlcU9' alt='404' />
+        <img style={{ height:'661px', width:'1365px' }} src='https://drive.google.com/uc?export=view&id=12S0JQ-ri_NoDbrDuKntzabEUXHFnlcU9' alt='404' />
     </div>
   )
 }
