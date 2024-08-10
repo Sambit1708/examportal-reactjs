d07b4e317eb4e56ed39cd4b71e023902f196be59
sambit1708 <sambitkhandai6@gmail.com>
2024-08-11 00:33:33 +0530

    [11-08-2024 00:33]_update_commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import HomePage from "./pages/HomePage/HomePage";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/SignUP/SignUpPage";
 import LoginPage from "./pages/Login/LoginPage";
 import UserDashboard from "./pages/user/UserDashboard";
@@ -14,85 +14,102 @@ import AdminQuizUpdate from "./pages/admin/Quiz/AdminQuizUpdate.jsx";
 import AdminQuestion from "./pages/admin/Questions/AdminQuestions";
 import AdminAddQuestion from "./pages/admin/Questions/AdminAddQuestion";
 import UpdateQuestion from "./pages/admin/Questions/UpdateQuestion";
-import UserPage from './pages/admin/Users/UserPage';
+import UserPage from "./pages/admin/Users/UserPage";
 import QuizAction from "./pages/user/Quizes/QuizAction";
 import ReadInstruction from "./pages/user/Quizes/ReadInstruction";
 import StartQuiz from "./pages/user/StartQuiz/StartQuiz";
 import Loading from "./components/Loading";
 import axios from "axios";
-import AdminProtectionRoute from './Utils/AdminProtectionRoute';
-import UserProtectRoutes from './Utils/UserProtectRoutes';
+import AdminProtectionRoute from "./Utils/AdminProtectionRoute";
+import UserProtectRoutes from "./Utils/UserProtectRoutes";
 import PageNotFound from "./pages/PageNotFound";
 import QuizResult from "./pages/user/StartQuiz/QuizResult";
-import './App.css'
+import "./App.css";
 import AdminResultPage from "./pages/admin/Result/AdminResultPage.jsx";
 
 function App() {
-
-  const [loading, setLoading] = React.useState(false)
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
     axios.interceptors.request.use((request) => {
-      setLoading(true)
+      setLoading(true);
       return request;
     });
 
     axios.interceptors.response.use((request) => {
-      setLoading(false)
+      setLoading(false);
       return request;
     });
-  }, [])
+  }, []);
 
   return (
     <React.Fragment>
       <Loading show={loading} />
       <Router>
-          <Routes>
-            <Route path="/">
-              <Route index element={<HomePage />}></Route>
-              <Route path='register' element={<SignUpPage />}></Route>
-              <Route path='login' element={<LoginPage />}></Route>
-              <Route element={<AdminProtectionRoute />}>
-                <Route path="Admin">
-                  <Route index element={<AdminDashboard />}></Route>
-                  <Route path='Profile' element={<AdminProfile />}></Route>
-                  <Route path='Users' element={<UserPage />}></Route>
-                  <Route path='Category'>
-                    <Route index element={<AdminCategories />}></Route>
-                    <Route path='Add' element={<AdminAddCategory />}></Route>
-                  </Route>
-                  <Route path='Quiz'>
+        <Routes>
+          <Route path="/">
+            <Route index element={<HomePage />}></Route>
+            <Route path="register" element={<SignUpPage />}></Route>
+            <Route path="login" element={<LoginPage />}></Route>
+            <Route element={<AdminProtectionRoute />}>
+              <Route path="Admin">
+                <Route index element={<AdminDashboard />}></Route>
+                <Route path="Profile" element={<AdminProfile />}></Route>
+                <Route path="Users" element={<UserPage />}></Route>
+                <Route path="Category">
+                  <Route index element={<AdminCategories />}></Route>
+                  <Route path="Add" element={<AdminAddCategory />}></Route>
+                </Route>
+                <Route path="Quiz">
+                  <Route index element={<AdminQuizes />}></Route>
+                  <Route path="Add" element={<AdminAddQuiz />}></Route>
+                  <Route
+                    path="Update/:id"
+                    element={<AdminQuizUpdate />}
+                  ></Route>
+                  <Route
+                    path="Question/:id"
+                    element={<AdminQuestion />}
+                  ></Route>
+                  <Route path="Question">
                     <Route index element={<AdminQuizes />}></Route>
-                    <Route path='Add' element={<AdminAddQuiz />}></Route>
-                    <Route path='Update/:id' element={<AdminQuizUpdate />}></Route>
-                    <Route path='Question/:id' element={<AdminQuestion />}></Route>
-                    <Route path='Question'>
-                      <Route index element={<AdminQuizes />}></Route>
-                      <Route path='Add/:id' element={<AdminAddQuestion />}></Route>
-                      <Route path='Update/:id' element={<UpdateQuestion />}></Route>
-                    </Route>
-                  </Route>
-                  <Route path="results">
-                    <Route index element={<AdminResultPage />}></Route>
-                    <Route path="by-quiz/:id" element={<AdminResultPage />}></Route>
+                    <Route
+                      path="Add/:id"
+                      element={<AdminAddQuestion />}
+                    ></Route>
+                    <Route
+                      path="Update/:id"
+                      element={<UpdateQuestion />}
+                    ></Route>
                   </Route>
                 </Route>
-              </Route>
-              <Route element={<UserProtectRoutes />}>
-                <Route path="User">
-                  <Route index element={<UserDashboard />}></Route>
-                  <Route path='Quiz'>
-                    <Route index element={<UserDashboard />}></Route>
-                    <Route path=':id' element={<QuizAction />}></Route>
-                    <Route path='instruction-page/:id' element={<ReadInstruction />}></Route>
-                    <Route path='Result/:id' element={<QuizResult />}></Route>
-                    <Route path='Start/:id' element={<StartQuiz />}></Route>
-                  </Route>
+                <Route path="results">
+                  <Route index element={<AdminResultPage />}></Route>
+                  <Route
+                    path="by-quiz/:id"
+                    element={<AdminResultPage />}
+                  ></Route>
                 </Route>
               </Route>
             </Route>
-            <Route path='*' element={<PageNotFound />}></Route>
-          </Routes>
+            <Route element={<UserProtectRoutes />}>
+              <Route path="User">
+                <Route index element={<UserDashboard />}></Route>
+                <Route path="Quiz">
+                  <Route index element={<UserDashboard />}></Route>
+                  <Route path=":id" element={<QuizAction />}></Route>
+                  <Route
+                    path="instruction-page/:id"
+                    element={<ReadInstruction />}
+                  ></Route>
+                  <Route path="Result/:id" element={<QuizResult />}></Route>
+                  <Route path="Start/:id" element={<StartQuiz />}></Route>
+                </Route>
+              </Route>
+            </Route>
+          </Route>
+          {/* <Route path='*' element={<PageNotFound />}></Route> */}
+        </Routes>
       </Router>
     </React.Fragment>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,42 +1,60 @@
-import * as React from 'react'
-import './HomePage.css'
-import NavBar from '../../components/NavBar'
-import { Box, Button,Container } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import "./HomePage.css";
+import NavBar from "../../components/NavBar";
+import { Box, Button, Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <React.Fragment>
-        <NavBar />
-        <Box sx={{ marginTop:'70px', height: '550px'}}>
-            <Box className='inner-home' >
-                <div style={{display:'flex'}}>
-                    <div className='home-text'>
-                        <h1 style={{fontSize: '4em', marginTop:'50px'}}><span>My</span> <span>Examportal</span></h1>
-                        <Container sx={{ margin: '50px 0 0 10px' }}>
-                            <Button sx={{   fontSize: '1.2em',
-                                            marginRight: '10px',
-                                            width:[150, 150, 200, 200, 200],
-                                            height: '50px',
-                                            borderRadius: '20px' }} 
-                                    variant='contained' >Visit Us
-                            </Button>
-                            <Button sx={{   fontSize: '1.2em',
-                                            width:[150, 150, 200, 200, 200],
-                                            height: '50px',
-                                            borderRadius: '20px' }} 
-                                    variant='contained' onClick={() => {navigate('/register')}}>Subscribe
-                            </Button>
-                        </Container>
-                    </div>
-                    <img style={{ width:'400px', marginTop: '-50px' }} src='https://res.cloudinary.com/djgwfxhqp/image/upload/v1721023887/ivrhjlhwxwkmxoeviljq.jpg' alt='exam' />
-                </div>
-            </Box>
+      <NavBar />
+      <Box sx={{ marginTop: "70px", height: "550px" }}>
+        <Box className="inner-home">
+          <div style={{ display: "flex" }}>
+            <div className="home-text">
+              <h1 style={{ fontSize: "4em", marginTop: "50px" }}>
+                <span>My</span> <span>Examportal</span>
+              </h1>
+              <Container sx={{ margin: "50px 0 0 10px" }}>
+                <Button
+                  sx={{
+                    fontSize: "1.2em",
+                    marginRight: "10px",
+                    width: [150, 150, 200, 200, 200],
+                    height: "50px",
+                    borderRadius: "20px",
+                  }}
+                  variant="contained"
+                >
+                  Visit Us
+                </Button>
+                <Button
+                  sx={{
+                    fontSize: "1.2em",
+                    width: [150, 150, 200, 200, 200],
+                    height: "50px",
+                    borderRadius: "20px",
+                  }}
+                  variant="contained"
+                  onClick={() => {
+                    navigate("/register");
+                  }}
+                >
+                  Subscribe
+                </Button>
+              </Container>
+            </div>
+            <img
+              style={{ width: "400px", marginTop: "-50px" }}
+              src="https://res.cloudinary.com/djgwfxhqp/image/upload/v1721023887/ivrhjlhwxwkmxoeviljq.jpg"
+              alt="exam"
+            />
+          </div>
         </Box>
+      </Box>
     </React.Fragment>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
