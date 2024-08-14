66cee01d308fcb0f0eafc1a4e6bdde14d224c154
sambit1708 <sambitkhandai6@gmail.com>
2024-08-14 21:06:59 +0530

    [14-08-2023 21:06]_update_commit

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -4,13 +4,13 @@ import axios from "axios";
  * ! This axios is created in order to add Interceptors and define baseURL.
  */
 const API = axios.create({
-  baseURL: "http://13.202.153.111:8085/",
+  baseURL: "https://13.202.153.111:8085/",
 });
 
 API.interceptors.request.use((request) => {
   if (
     localStorage.getItem("token") != null &&
-    request.url !== "http://13.202.153.111:8085/generate-token"
+    request.url !== "https://13.202.153.111:8085/generate-token"
   ) {
     request.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   }
